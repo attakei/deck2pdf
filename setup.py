f777256aab9c47ff4d25fb6c55d2d4d310db251b
attakei <attakei@gmail.com>
2015-10-19 10:42:51 +0900

    Mod classifiers in README.

--- setup.py
+++ setup.py
@@ -1,21 +1,36 @@
+import os
 from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 package_requires = [
     'selenium',
     'reportlab',
 ]
 
+# Use README.rst for long description.
+readme_path = os.path.join(here, 'README.rst')
+long_description = ''
+if os.path.exists(readme_path):
+    with open(readme_path) as fp:
+        long_description = fp.read()
+
+
 setup(
     name='slide2pdf',
     version='0.0.1',
     url='https://github.com/attakei/slide2pdf',
     description='Convert html5-slide into pdf',
-    long_description='',
+    long_description=long_description,
     author='attakei',
     author_email='attakei@gmail.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
     ],
     keywords='html5slide pdf',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
