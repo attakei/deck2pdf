f2025d8646d39774ebb08ebd0ba7535fd1ddf0c0
attakei <attakei@gmail.com>
2015-10-22 02:04:45 +0900

    Mod test to run right.

--- tests/test_it.py
+++ tests/test_it.py
@@ -13,6 +13,6 @@ class TestForMain(unittest.TestCase):
 
     def test_help(self):
         with self.assertRaises(SystemExit):
-            slide2pdf.main()
+            slide2pdf.main([])
         with self.assertRaises(SystemExit):
             slide2pdf.main(['-h'])
