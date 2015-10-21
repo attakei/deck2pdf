014c590247ba13b5467f0373a3b70bdcaaa6d211
attakei <attakei@gmail.com>
2015-10-21 18:26:38 +0900

    time control.

--- slide2pdf.py
+++ slide2pdf.py
@@ -61,9 +61,8 @@ def main():
 
         # Open Web Browser & Resize 720P
         driver.get(url_)
-        time.sleep(1)
         driver.refresh()
-        time.sleep(1)
+        time.sleep(2)
 
         # Get Screen Shot
         driver.save_screenshot(FILENAME)
