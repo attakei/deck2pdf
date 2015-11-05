26348808e7e1b0f06367772593c943c0985aba25
attakei <attakei@gmail.com>
2015-11-05 16:57:53 +0900

    In CircleCI, skip test for PhantomJS and Ghost.py

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 import slide2pdf
-from pytest import raises
+from pytest import raises, mark
 
 
 current_dir = os.path.abspath(os.getcwd())
 test_dir = os.path.abspath(os.path.dirname(__file__))
+skip_in_ci = mark.skipif("'FULL_TEST' not in os.environ")
 
 
 class TestForMain(object):
@@ -15,7 +16,8 @@ class TestForMain(object):
     def test_help(self):
         raises(SystemExit, slide2pdf.main, [])
         raises(SystemExit, slide2pdf.main, ['-h'])
-
+    
+    @skip_in_ci
     def test_files(self):
         test_slide_path = os.path.join(test_dir, 'testslide/_build/slides/index.html')
         slide2pdf.main([test_slide_path, ])
