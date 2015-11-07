8d8c68bd7f48a906fc4a536f061bb0782d2e2756
attakei <attakei@gmail.com>
2015-11-08 03:01:54 +0900

    If test use stub for capture engine, do not need testslide

--- tests/test_it.py
+++ tests/test_it.py
@@ -3,7 +3,6 @@ import deck2pdf
 from pytest import raises
 from . import (
     current_dir,
-    test_dir,
 )
 
 
@@ -12,14 +11,16 @@ def test_help():
     raises(SystemExit, deck2pdf.main, ['-h'])
 
 
-def test_files():
-    test_slide_path = os.path.join(test_dir, 'testslide/_build/slides/index.html')
+def test_output_default():
+    test_slide_path = os.path.join(__file__)
     deck2pdf.main([test_slide_path, '-c', 'stub'])
     assert os.path.exists(os.path.join(current_dir, '.deck2pdf'))
+    assert os.path.exists(os.path.join(current_dir, 'slide.pdf'))
 
 
 def test_output_file_by_name():
     output_path = os.path.join(current_dir, '.deck2pdf', 'test.output')
-    test_slide_path = os.path.join(test_dir, 'testslide/_build/slides/index.html')
+    test_slide_path = os.path.join(__file__)
     deck2pdf.main([test_slide_path, '-c', 'stub', '-o', output_path])
+    assert os.path.exists(os.path.join(current_dir, '.deck2pdf'))
     assert os.path.exists(output_path)
