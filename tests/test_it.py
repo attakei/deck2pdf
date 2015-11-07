4e89e6b5e3e4e2674179a6d6914d21548f02f8d8
attakei <attakei@gmail.com>
2015-11-08 02:59:27 +0900

    Refactor test
    deck2pdf.main() delete temporary save directory in running.
    So test need not to delete it

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import deck2pdf
 from pytest import raises
 from . import (
@@ -8,21 +7,19 @@ from . import (
 )
 
 
-class TestForMain(object):
-    def setUp(self):
-        shutil.rmtree(os.path.join(current_dir, '.deck2pdf'), ignore_errors=True)
+def test_help():
+    raises(SystemExit, deck2pdf.main, [])
+    raises(SystemExit, deck2pdf.main, ['-h'])
 
-    def test_help(self):
-        raises(SystemExit, deck2pdf.main, [])
-        raises(SystemExit, deck2pdf.main, ['-h'])
 
-    def test_files(self):
-        test_slide_path = os.path.join(test_dir, 'testslide/_build/slides/index.html')
-        deck2pdf.main([test_slide_path, '-c', 'stub'])
-        assert os.path.exists(os.path.join(current_dir, '.deck2pdf'))
+def test_files():
+    test_slide_path = os.path.join(test_dir, 'testslide/_build/slides/index.html')
+    deck2pdf.main([test_slide_path, '-c', 'stub'])
+    assert os.path.exists(os.path.join(current_dir, '.deck2pdf'))
 
-    def test_output_file_by_name(self):
-        output_path = os.path.join(current_dir, '.deck2pdf', 'test.output')
-        test_slide_path = os.path.join(test_dir, 'testslide/_build/slides/index.html')
-        deck2pdf.main([test_slide_path, '-c', 'stub', '-o', output_path])
-        assert os.path.exists(output_path)
+
+def test_output_file_by_name():
+    output_path = os.path.join(current_dir, '.deck2pdf', 'test.output')
+    test_slide_path = os.path.join(test_dir, 'testslide/_build/slides/index.html')
+    deck2pdf.main([test_slide_path, '-c', 'stub', '-o', output_path])
+    assert os.path.exists(output_path)
