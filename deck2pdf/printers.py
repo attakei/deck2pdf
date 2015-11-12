f5965ebfe99777078fedc9524de54941943cf1c9
attakei <attakei@gmail.com>
2015-11-12 22:40:22 +0900

    Divide test

--- deck2pdf/printers.py
+++ deck2pdf/printers.py
@@ -9,13 +9,12 @@ __author__ = 'attakei'
 def calc_filled_pagesize(origin, capture):
     """
     """
-    origin_ratio = origin[0] / origin[1]
     capture_ratio = float(capture[0]) / float(capture[1])
     pagesize = [0, 0]
-    if origin_ratio > capture_ratio:
-        pagesize[0] = origin[0] * origin_ratio / capture_ratio
+    if origin[0] > origin[1]:
+        pagesize[0] = origin[1] * capture_ratio
         pagesize[1] = origin[1]
     else:
         pagesize[0] = origin[0]
-        pagesize[1] = origin[1] * origin_ratio / capture_ratio
+        pagesize[1] = origin[0] / capture_ratio
     return pagesize
--- tests/test_printers.py
+++ tests/test_printers.py
@@ -10,8 +10,16 @@ __author__ = 'attakei'
 
 
 def test_mod_scale():
+    size_origin = A4
+    capture_size = (400, 300)
+    fixed_size = printers.calc_filled_pagesize(size_origin, capture_size)
+    assert fixed_size[0] == size_origin[0]
+    assert capture_size[0]*fixed_size[1] == capture_size[1]*fixed_size[0]
+
+
+def test_mod_scale_landscape():
     size_origin = landscape(A4)
     capture_size = (400, 300)
     fixed_size = printers.calc_filled_pagesize(size_origin, capture_size)
-    assert capture_size[0]*fixed_size[1] == capture_size[1]*fixed_size[0]
     assert fixed_size[1] == size_origin[1]
+    assert capture_size[0]*fixed_size[1] == capture_size[1]*fixed_size[0]
